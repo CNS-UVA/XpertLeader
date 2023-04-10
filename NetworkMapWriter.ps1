63a4c0769cb8ff6c7e5b3c435bc97c37f5b545de
Grant Matteo <matteoboy3@gmail.com>
2023-04-10 16:52:19 -0400

    Add Service Checking

--- NetworkMapWriter.ps1
+++ NetworkMapWriter.ps1
@@ -29,6 +29,31 @@ Function Build-Subnet {
     Return $ips
 
 }
+
+Function Find-Services {
+    param(
+        $IPs,
+        $IncludeServices= @()
+    )
+    
+    $activeServices = @{} # ip -> services
+    $ips | % { $activeServices[$_] = @() }
+
+    $serviceNameToPort.GetEnumerator() | ? {$_.Key -in $IncludeServices} | % { 
+        
+        New-NetFirewallRule -DisplayName "servicetest" -Name "servicetest" -Protocol TCP -Direction Outbound -Action Allow -RemotePort $_.Value 
+        Foreach ($ip in $IPs) {
+            $connResult = Test-NetConnection -ComputerName $ip -Port $_.Value
+            If ($connResult.TcpTestSucceeded) {
+                $activeServices[$ip] += $_.Value
+            }
+        }
+
+    }
+    Return $activeServices
+}
+
+
 Write-Host "To use this, type the prefix for your subnet, then the ips in it. If you can't figure it out, ask Grant."
 $ips = @()
 $subnets = @()
@@ -41,6 +66,47 @@ While ($true) {
     $ips += $subnet
 } 
 
+$serviceNameToPort = @{
+        "smtp" = 25;
+        "pop3" = 110;
+        "imap" = 143;
+        "ftp" = 21;
+        "ssh" = 22;
+        "smb" = 445;
+        "rpc" = 135;
+        "winrm" = 5985;
+        "winrms" = 5986
+        "mysql" = 3306;
+        "postgresql" = 5432;
+        "nfs" = 111;
+        "http" = 80;
+        "https" = 443
+        "rdp" = 3389;
+        "ldap" = 389;
+        "dns" = 53;
+        }
+$servicePortToName = @{}
+$serviceNameToPort.GetEnumerator() | % { $servicePortToName[$_.Value] = $_.Key }
+
+
+Write-Host "Scan Network for services? If your name matches /^[^G]/, this is not recommended."
+$scanForServices = Read-Host "y/N"
+
+$services = @()
+If ($scanForServices -eq "y") {
+    While ($true) {
+        $service = Read-Host "Service Name?"
+        If ($service -eq "") {
+           Break
+        }
+        If ($service -in $serviceNameToPort.Keys) {
+            $services += $service
+        } Else {
+            Write-Host ("Wrong Service. Valid Services are:`n" + $serviceNameToPort.Keys)
+        }
+    } 
+    $activeServices = Find-Services -IPs $ips -IncludeServices $services
+}
 
 
 $ordinals = @("first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth")
@@ -52,6 +118,16 @@ Foreach ($i in 0..($subnets.Length-1)) {
     $cidr = $subnets[$i][0] -replace "\d+$","0/24" # TODO extend to more than /24 networks
     Write-Host ("`nThe " + $ordinals[$i] + " subnet is $cidr. It contains the following machines:")
     Foreach ($ip in $subnets[$i]) {
-        Write-Host "    $ip"
+        Write-Host "    $ip" -NoNewline
+        If ($services -ne @()) {
+            Write-Host ", which is running the following services:"  
+            Foreach ($servicePort in $activeServices[$ip]) {
+                Write-Host ("        " + $servicePortToName[$servicePort] +" ($servicePort)")        
+            }
+        
+            
+        }
+        Write-Host ""
     }
 }
+
